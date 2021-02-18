97245d88cc6587600d3db6ae46e25d35cfe93ab6
rahuldnic <76942182+rahuldnic@users.noreply.github.com>
2021-02-18 15:42:24 +0530

    Update ValidationHandler.java

--- ValidationHandler.java
+++ ValidationHandler.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.nic.epanjeeyan;
+package in.rd.epanjeeyan;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
